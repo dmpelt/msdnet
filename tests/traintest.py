b4c78ff35757a7481110c2a82da586527891b8d6
Daniel M. Pelt <D.M.Pelt@cwi.nl>
2019-07-17 12:40:46 +0200

    Add unit tests

---
+++ tests/traintest.py
@@ -0,0 +1,63 @@
+import unittest
+import msdnet
+import numpy as np
+
+class TestTraining(unittest.TestCase):
+
+    def test_regr_training(self):
+        dil = msdnet.dilations.IncrementDilations(3)
+        n = msdnet.network.MSDNet(10, dil, 1, 1, gpu=False)
+        n.initialize()
+        tgt_im = np.zeros((1,128,128), dtype=np.float32)
+        tgt_im[:,32:96,32:96] = 1
+        inp_im = (tgt_im + np.random.normal(size=tgt_im.shape)).astype(np.float32)
+        d = msdnet.data.ArrayDataPoint(inp_im, tgt_im)
+        bprov = msdnet.data.BatchProvider([d,],1)
+        val = msdnet.validate.MSEValidation([d,])
+        t = msdnet.train.AdamAlgorithm(n)
+        n.normalizeinout([d,])
+        log1 = msdnet.loggers.ConsoleLogger()
+        log2 = msdnet.loggers.FileLogger('test_log_regr.txt')
+        log3 = msdnet.loggers.ImageLogger('test_log_regr')
+        for i in range(10):
+            t.step(n, bprov.getbatch())
+            t.to_file('test_regr_params.h5')
+            val.to_file('test_regr_params.h5')
+            n.to_file('test_regr_params.h5')
+            val.validate(n)
+            log1.log(val)
+            log2.log(val)
+            log3.log(val)
+        n_load = msdnet.network.MSDNet.from_file('test_regr_params.h5')
+        n_load.forward(inp_im)
+    
+    def test_segm_training(self):
+        dil = msdnet.dilations.IncrementDilations(3)
+        n = msdnet.network.SegmentationMSDNet(10, dil, 1, 2, gpu=False)
+        n.initialize()
+        tgt_im = np.zeros((1,128,128), dtype=np.uint8)
+        tgt_im[:,32:96,32:96] = 1
+        inp_im = (tgt_im + np.random.normal(size=tgt_im.shape)).astype(np.float32)
+        d = msdnet.data.ArrayDataPoint(inp_im, tgt_im)
+        d = msdnet.data.OneHotDataPoint(d,[0,1])
+        bprov = msdnet.data.BatchProvider([d,],1)
+        val = msdnet.validate.MSEValidation([d,])
+        t = msdnet.train.AdamAlgorithm(n)
+        n.normalizeinout([d,])
+        log1 = msdnet.loggers.ConsoleLogger()
+        log2 = msdnet.loggers.FileLogger('test_log_segm.txt')
+        log3 = msdnet.loggers.ImageLabelLogger('test_log_segm')
+        for i in range(10):
+            t.step(n, bprov.getbatch())
+            t.to_file('test_segm_params.h5')
+            val.to_file('test_segm_params.h5')
+            n.to_file('test_segm_params.h5')
+            val.validate(n)
+            log1.log(val)
+            log2.log(val)
+            log3.log(val)
+        n_load = msdnet.network.MSDNet.from_file('test_segm_params.h5')
+        n_load.forward(inp_im)
+
+if __name__ == '__main__':
+    unittest.main()
